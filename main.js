82942bc6946895fd7267594bac1e1eb826b77ab9
Winston Calhoun <superdevant@gmail.com>
2022-12-16 09:00:58 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// Login Page
+
+// Refactor your application so that the user lands on a “Login” page
+// The login page should match the style of the application.
+// The login page should contain an input for a user to enter their name, and a button to submit.
+// After the user clicks the submit button, they should be taken to the main application page, and see a personalized greeting that displays their name and some sort of welcome message.
+// The welcome message and name should appear in a logical place of your choosing.
