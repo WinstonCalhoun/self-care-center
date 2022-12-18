c9203cd363df8ef2ae240d02ba8d9a2dcbd7206f
Winston Calhoun <superdevant@gmail.com>
2022-12-18 15:24:13 -0600

    Initial commit: variables added

--- main.js
+++ main.js
@@ -5,3 +5,26 @@
 // The login page should contain an input for a user to enter their name, and a button to submit.
 // After the user clicks the submit button, they should be taken to the main application page, and see a personalized greeting that displays their name and some sort of welcome message.
 // The welcome message and name should appear in a logical place of your choosing.
+
+// Global Variables
+var affirmations[];
+var montras[];
+ 
+// Query Selectors
+var radio1 = document.querySelector('#radio1');
+var radio1 = document.querySelector('#radio1');
+var loginPage = document.querySelector('.login-page')
+var mainPage = document.querySelector('.selection-box');
+
+// Buttons
+var makeNewBtn = document.querySelector('.make-new-message');
+var loginBtn = document.querySelector('.login-page');
+
+// Event Listeners
+makeNewBtn.addEventListener('click', );
+loginBtn.addEventListener('click', );
+
+document.addEventListener('DOMContentLoaded', function() {
+    ();
+    ();
+  });
